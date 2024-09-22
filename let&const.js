c8dc603469881fcbb1a01aa71b3e34d4a3673662
divyasaireddy <divyasaireddy0910@gmail.com>
2024-09-22 23:07:47 +0530

    variables types related files added

---
+++ let&const.js
@@ -0,0 +1,24 @@
+{
+    var a = 10;
+    var a=30;
+    console.log(a);  
+}
+
+let d=40;
+console.log(d);
+
+const e=50;
+console.log(e);
+
+{
+    let b = 20;
+    b=30;
+    console.log(b);
+
+
+}
+{
+    const c = 30;
+    console.log(c);
+
+}
